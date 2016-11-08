ba6a9e33944ed766d88e09c6564c36f6087fcfc2
cp2091991 <chandraprakash.cp9@gmail.com>
2016-11-08 12:58:24 +0530

    created the subscriber form

--- subscribers/views.py
+++ subscribers/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def search(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            s_query = form.cleaned_data['search_query']
+            s_results = SomeTable.objects.filter(name=s_query)
+            return render(request, 'search.html', {'form': form, 's_results': s_results})
+    else:
+        form = SearchForm()
+
+    return render(request, 'search.html', {'form': form,})
