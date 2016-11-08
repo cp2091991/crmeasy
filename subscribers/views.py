4a403edf9c9d42928a213d04f0726eec8dacab8d
cp2091991 <chandraprakash.cp9@gmail.com>
2016-11-08 13:51:54 +0530

    created the subscriber view

--- subscribers/views.py
+++ subscribers/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
+from .forms import SubscriberForm
 def search(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
@@ -11,3 +14,23 @@ def search(request):
         form = SearchForm()
 
     return render(request, 'search.html', {'form': form,})
+def subscriber_new(request, template='subscribers/subscriber_new.html'):
+    if request.method == 'POST':
+        form = SubscriberForm(request.POST)
+        if form.is_valid():
+            # Unpack form values
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            email = form.cleaned_data['email']
+            # Create the User record
+            user = User(username=username, email=email)
+            user.set_password(password)
+            user.save()
+            # Create Subscriber Record
+            # Process payment (via Stripe)
+            # Auto login the user
+            return HttpResponseRedirect('/success/')
+    else:
+        form = SubscriberForm()
+
+    return render(request, template, {'form':form})
