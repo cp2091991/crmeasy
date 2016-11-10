4eb8777f653e528a961305541be32281150700f7
cp2091991 <chandraprakash.cp9@gmail.com>
2016-11-10 12:39:50 +0530

    created the contact detail url

--- crmapp/urls.py
+++ crmapp/urls.py
@@ -24,6 +24,7 @@ from accounts.urls import account_urls
 from django.conf import settings
 from django.conf.urls.static import static
 from accounts.urls import account_urls
+from contacts.urls import contact_urls
 admin.autodiscover()
 
 urlpatterns = [
@@ -33,10 +34,11 @@ urlpatterns = [
      url(r'^admin/', include(admin.site.urls)),
      url(regex=r'^login/$', view=login, kwargs={'template_name': 'login.html'},name='login'),
      url(regex=r'^logout/$', view=logout, kwargs={'next_page': '/'}, name='logout'),
-     
+
      url(r'^account/new/$',account_cru, name='account_new'),
      url(r'^account/list/$',AccountList.as_view(), name='account_list'),
      url(r'^account/(?P<uuid>[\w-]+)/', include(account_urls)),
+     url(r'^contact/(?P<uuid>[\w-]+)/', include(contact_urls)),
 
 
 
