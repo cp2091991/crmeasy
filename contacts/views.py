dbedb326314db4779e26244e4720bc959afc50f5
cp2091991 <chandraprakash.cp9@gmail.com>
2016-11-10 14:07:37 +0530

    activated AJAX for contact edit/new features

--- contacts/views.py
+++ contacts/views.py
@@ -40,14 +40,25 @@ def contact_cru(request, uuid=None, account=None):
             contact.owner = request.user
             contact.save()
             # return the user to the account detail view
-            reverse_url = reverse(
-                'account_detail',
-                args=(account.uuid,)
-            )
-            return HttpResponseRedirect(reverse_url)
-    else:
-            # if the form isn't valid, still fetch the account so it can be passed to the template
-            account = form.cleaned_data['account']
+            if request.is_ajax():
+                return render(request,
+                              'contacts/contact_item_view.html',
+                              {'account':account, 'contact':contact}
+                )
+            else:
+                reverse_url = reverse(
+                    'crmapp.accounts.views.account_detail',
+                    args=(account.uuid,)
+                )
+                return HttpResponseRedirect(reverse_url)
+        #     reverse_url = reverse(
+        #         'account_detail',
+        #         args=(account.uuid,)
+        #     )
+        #     return HttpResponseRedirect(reverse_url)
+        # else:
+        #     # if the form isn't valid, still fetch the account so it can be passed to the template
+        #     account = form.cleaned_data['account']
     else:
         form = ContactForm(instance=contact)
 
