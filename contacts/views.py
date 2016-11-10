008c440c7a669a3e8644df6330943c7bdddf2074
cp2091991 <chandraprakash.cp9@gmail.com>
2016-11-10 14:00:23 +0530

    modified contact view to allow for editing

--- contacts/views.py
+++ contacts/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
 from .models import Contact
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseForbidden
-
 from .forms import ContactForm
+from django.shortcuts import get_object_or_404
+from crmapp.accounts.models import Account
 
 @login_required()
 def contact_detail(request, uuid):
@@ -21,10 +19,17 @@ def contact_detail(request, uuid):
                 {'contact': contact}
     )
 @login_required()
-def contact_cru(request):
+def contact_cru(request, uuid=None, account=None):
+
+    if uuid:
+        contact = get_object_or_404(Contact, uuid=uuid)
+        if contact.owner != request.user:
+            return HttpResponseForbidden()
+    else:
+        contact = Contact(owner=request.user)
 
     if request.POST:
-        form = ContactForm(request.POST)
+        form = ContactForm(request.POST, instance=contact)
         if form.is_valid():
             # make sure the user owns the account
             account = form.cleaned_data['account']
@@ -41,10 +46,19 @@ def contact_cru(request):
             )
             return HttpResponseRedirect(reverse_url)
     else:
-        form = ContactForm()
+            # if the form isn't valid, still fetch the account so it can be passed to the template
+            account = form.cleaned_data['account']
+    else:
+        form = ContactForm(instance=contact)
+
+    # this is used to fetch the account if it exists as a URL parameter
+    if request.GET.get('account', ''):
+        account = Account.objects.get(id=request.GET.get('account', ''))
 
     variables = {
         'form': form,
+        'contact': contact,
+        'account': account
     }
 
     template = 'contacts/contact_cru.html'
